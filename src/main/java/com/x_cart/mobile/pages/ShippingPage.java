b24c332f27203569a16e9a136073750bcce9e161
Chintan170218 <chintantiwari7@gmail.com>
2022-05-04 00:55:50 +0100

    xcartdemo-maven

---
+++ src/main/java/com/x_cart/mobile/pages/ShippingPage.java
@@ -0,0 +1,15 @@
+package com.x_cart.mobile.pages;
+
+import com.x_cart.mobile.utility.Utility;
+import org.openqa.selenium.By;
+
+public class ShippingPage extends Utility {
+
+    By shippingText = By.id("page-title");
+
+
+    public String getShippingText(){
+        return getTextFromElement(shippingText);
+    }
+
+}
